050d6c2483cdcef4a603cf9fe78f1ecf6e6e762d
gordchan <gordon.chan@mensa.org.hk>
2015-08-17 02:32:20 +0000

    Frequency of events included

--- RepData2.R
+++ RepData2.R
@@ -1,14 +1,29 @@
-## Loading library
+health_c <- count(health, StdName)
+econ_c <- count(econ, StdName)
 
-library(dplyr)
+names(health_c) <- c("Event", "Occurance")
+names(econ_c) <- c("Event", "Occurance")
 
+colour_h <- brewer.pal(3, "Set1")
+    colour_h <- colour_h[2]
 
-## Loading dataset
+colour_e <- brewer.pal(3, "Set2")
+    colour_e <- colour_e[1]
 
-storm <- read.csv("repdata-data-StormData.csv.bz2")
+h<-ggplot(health_c, aes(x=Event, y=Occurance))+
+  geom_histogram(stat = "identity", fill = colour_h)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(legend.position="none")+
+  xlab("")+
+  annotate("text", x = 10, y = 2500, label = "Health-endangering Events")+
+  annotate("rect", xmin = 5, xmax = 15, ymin = 2300, ymax = 2700, alpha = .1)
 
-storm <- group_by(storm, EVTYPE)
+e<-ggplot(econ_c, aes(x=Event, y=Occurance, fill=colour_e))+
+  geom_histogram(stat = "identity", fill = colour_e)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(legend.position="none")+
+  xlab("")+
+  annotate("text", x = 11, y = 90000, label = "Damage-causing Events")+
+  annotate("rect", xmin = 5, xmax = 17, ymin = 82000, ymax = 98000, alpha = .1)
 
-health <- select(storm, BGN_DATE, EVTYPE, FATALITIES, INJURIES)
-
-econ <- select(storm, BGN_DATE, PROPDMG, PROPDMGEXP, CROPDMG, CROPDMGEXP)
+grid.arrange(h, e, ncol = 1, top="Fig.2 - Occurance of health-endangering/damage-causing Storm Events", bottom="Storm Event Type")
